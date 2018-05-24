c9bbad0a7fb1e4f587ce6ada9d17bc308ea6e9af
Flademir <fladermorais@hotmail.com>
2018-05-24 20:34:20 -0300

    Colocando meu nome para fazer teste no Net Beans

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-	echo "Meu primeiro código PHP! Hello WORLD";
+$nome = "Flademir";
+$sobrenome = "Morais";
+
+echo "Meu primeiro código PHP! Hello WORLD <br>";
+
+echo $nome . ' é o meu primeiro nome, meu sobrenome é ' . $sobrenome;
+echo "<br>Ou seja, meu nome completo é: " . $nome . " " . $sobrenome;
 ?>
